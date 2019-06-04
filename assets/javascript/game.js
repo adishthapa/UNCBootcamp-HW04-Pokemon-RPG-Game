a9efdb7d59e132aac13f319f5d15c6b17086fd69
adishthapa <adishthapa@outlook.com>
2019-06-03 22:11:06 -0400

    Created a basic layout for the intro of the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$("#bulbasaur-js").on("click", function() {
+    $("body").html("");
+});
+
+$("#squirtle-js").on("click", function() {
+    $("body").html("");
+});
+
+$("#charmander-js").on("click", function() {
+    $("body").html("");
+});
+
+$("#pikachu-js").on("click", function() {
+    $("body").html("");
+});
