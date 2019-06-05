ae640cd208797ef931c5b08e9bbe3b25df7a8aff
adishthapa <adishthapa@outlook.com>
2019-06-05 00:00:21 -0400

    Added comments to the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
 
+    // The name of the player's Pokemon
     var player;
-    var player_hp;
+    // The max hp of the player's Pokemon
     var max_player_hp;
+    // The current hp of the player's Pokemon
+    var player_hp;
+    // The initial attack damage of the player's Pokemon
     var player_attack;
+    // The current attack damage of the player's Pokemon
     var current_attack;
+    // The name of the cpu Pokemon
     var cpu;
-    var cpu_hp;
+    // The max hp of the cpu Pokemon
     var max_cpu_hp;
+    // The current hp of the cpu Pokemon
+    var cpu_hp;
+    // The counter attack damage of the cpu Pokemon
     var cpu_counter;
+    // An array that keeps track of the fights so far
     var fights = [];
+    // Checks to see if there is a fight going on or not
     var fighting = false;
+    // Checks to see if the current game has ended or not
     var end = false;
 
+    // Bulbasaur Object
     var bulbasaur = {
         name : "Bulbasaur",
         hp: 100,
@@ -24,6 +37,19 @@ $(document).ready(function(){
         sound: "assets/sounds/bulbasaur.wav"
     }
 
+     // Charmander Object
+     var charmander = {
+        name: "Charmander",
+        hp: 150,
+        attack: 5,
+        counter: 20,
+        img: "assets/images/charmander.png",
+        bg: "assets/images/fire.gif",
+        color: "#F47931",
+        sound: "assets/sounds/charmander.wav"
+    }
+
+    // Squirtle Object
     var squirtle = {
         name: "Squirtle",
         hp: 120,
@@ -35,17 +61,7 @@ $(document).ready(function(){
         sound: "assets/sounds/squirtle.wav"
     }
 
-    var charmander = {
-        name: "Charmander",
-        hp: 150,
-        attack: 5,
-        counter: 20,
-        img: "assets/images/charmander.png",
-        bg: "assets/images/fire.gif",
-        color: "#F47931",
-        sound: "assets/sounds/charmander.wav"
-    }
-
+    // Pikachu Object
     var pikachu = {
         name: "Pikachu",
         hp: 180,
@@ -57,6 +73,7 @@ $(document).ready(function(){
         sound: "assets/sounds/pikachu.mp3"
     }
 
+    // Function that resets the game
     function reset() {
         document.getElementById("background-music").setAttribute("src", "assets/sounds/title.mp3");
         $("body").css({
@@ -76,6 +93,7 @@ $(document).ready(function(){
         end = false;
     }
 
+    // Function that occurs when the player chooses their Pokemon
     function startGame(pokemon) {
         document.getElementById("background-music").setAttribute("src", "assets/sounds/battle.mp3");
         document.getElementById("pokemon-sound").setAttribute("src", pokemon.sound);
@@ -105,6 +123,7 @@ $(document).ready(function(){
         $(".main").show();
     };
 
+    // Function that occurs when the player chooses their Opponent
     function setFighter(pokemon) {
         document.getElementById("pokemon-sound").setAttribute("src", pokemon.sound);
         $("#comments").html("");
@@ -139,6 +158,7 @@ $(document).ready(function(){
         }
     }
 
+    // Function that occurs when the player attacks their Opponent
     function battle() {
         cpu_hp -= current_attack;
         player_hp -= cpu_counter;
@@ -176,49 +196,58 @@ $(document).ready(function(){
         }
     }
 
+    // Initial reset of the game
     reset();
 
-
+    // Looks for initial selection of Bulbasaur
     $(".bulbasaur-js1").on("click", function() {
         startGame(bulbasaur);
     });
 
+    // Looks for initial selection of Charmander
     $(".charmander-js1").on("click", function() {
         startGame(charmander);
     });
 
+    // Looks for initial selection of Squirtle
     $(".squirtle-js1").on("click", function() {
         startGame(squirtle);
     });
 
+    // Looks for initial selection of Pikachu
     $(".pikachu-js1").on("click", function() {
         startGame(pikachu);
     });
 
+    // Looks for opponent selection of Bulbasaur
     $(".bulbasaur-js2").on("click", function() {
         if (fighting === false) {
             setFighter(bulbasaur);
         }
     });
 
+    // Looks for opponent selection of Charmander
     $(".charmander-js2").on("click", function() {
         if (fighting === false) {
             setFighter(charmander);
         }
     });
 
+    // Looks for opponent selection of Squirtle
     $(".squirtle-js2").on("click", function() {
         if (fighting === false) {
             setFighter(squirtle);
         }
     });
 
+    // Looks for opponent selection of Pikachu
     $(".pikachu-js2").on("click", function() {
         if (fighting === false) {
             setFighter(pikachu);
         }
     });
 
+    // Looks for attack made by the player
     $("#attack").on("click", function() {
         if (end) {
             reset();
